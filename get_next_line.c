772ce94540621f812bd8fa9c91f95a111a3668a6
Antti Vornanen <avornane@c1r1p3.hive.fi>
2019-11-15 13:32:33 +0200

    fixed norm errors

--- get_next_line.c
+++ get_next_line.c
@@ -12,49 +12,49 @@
 
 #include "get_next_line.h"
 #include "libft/libft.h"
-#include <stdio.h> //TODO remove
 
-int find_next_line(const char **str, char ***l_str) //TODO add return type to int
-//TODO modify list pointer here
+int				find_next_line(const char **str, char ***l_str)
 {
-	char *line;
-	int i;
-	int result;
+	char		*line;
+	int			i;
+	int			result;
 
 	i = 0;
 	result = 0;
-	while((*str)[i] != '\0' && (*str)[i] != '\n')
+	if ((*str)[0] == '\0')
 	{
-		if((*str)[i] == '\0')
+		**l_str = "";
+		return (1);
+	}
+	while ((*str)[i] != '\0' && (*str)[i] != '\n')
+	{
+		if ((*str)[i] == '\0')
 			result = 1;
 		i++;
 	}
 	**l_str = ft_strsub(*str, 0, i);
-	while((i--) >= 0)
+	while ((i--) >= 0)
 		(*str)++;
-	return(result);
+	return (result);
 }
 
-
-int	get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	int ret;
-	char buf[BUFF_SIZE +1];
-	const char *str[FD_SIZE];
-	char *tmp;
+	int			ret;
+	char		buf[BUFF_SIZE + 1];
+	const char	*str[FD_SIZE];
+	char		*tmp;
 
-	if (fd > FD_SIZE && !line) //TODO error check
+	if (fd > FD_SIZE && !line)
 	{
 		write(2, "Error", 6);
 		return (-1);
 	}
-	while((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
 		if (str[fd] == NULL)
-		{
 			str[fd] = ft_strdup(buf);
-		}
 		else
 		{
 			tmp = ft_strjoin(str[fd], buf);
@@ -62,8 +62,5 @@ int	get_next_line(const int fd, char **line)
 			str[fd] = tmp;
 		}
 	}
-	if (str[fd][0] != '\0')
-		return (find_next_line(&str[fd], &line));
-	else
-		return (1);
+	return (find_next_line(&str[fd], &line));
 }
